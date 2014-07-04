b23e791a8ca2dd2d1960e40bcdf4f2c1598dc748
Evgeny Chistyakov <z_chiz@live.com>
2014-07-03 18:19:02 -0700

    Tested functionality
    -HttpGet now works (tested)
    -Login function works (tested)

--- WrapperCielo24/WrapperCielo24/WebUtils.cs
+++ WrapperCielo24/WrapperCielo24/WebUtils.cs
@@ -8,64 +8,32 @@ using System.Threading;
 
 namespace WrapperCielo24
 {
-    /*private class SynchronousHttpWebRequest : HttpWebRequest
-    {
-        private static const TimeSpan BASIC_TIMEOUT = new TimeSpan(TimeSpan.TicksPerSecond * 60); // 60 seconds
-        private static const TimeSpan DOWNLOAD_TIMEOUT = new TimeSpan(TimeSpan.TicksPerMinute * 5);  // 5 minutes
-        private AutoResetEvent allDone2 = new AutoResetEvent(false);
-
-        public void GetResponse()
-        {
-
-        }
-
-        private void GetResponseCallback(IAsyncResult result)
-        {
-            RequestState state = (RequestState)result.AsyncState;
-            WebResponse response = state.Request.EndGetResponse(result);
-            response.ToString();
-        }
-
-        private void GetRequestStreamCallback(IAsyncResult result)
-        {
-            RequestState state = (RequestState)result.AsyncState;
-            Stream postStream = state.Request.EndGetRequestStream(result);
-            postStream.Write(state.Buffer, 0, state.BufferSize);
-            postStream.Dispose();
-        }
-
-    }*/
-
     class WebUtils
     {
         // Progress events/properties?
-        // private static AutoResetEvent allDone = new AutoResetEvent(false);
         private static TimeSpan BASIC_TIMEOUT = new TimeSpan(TimeSpan.TicksPerSecond * 60); // 60 seconds
         private static TimeSpan DOWNLOAD_TIMEOUT = new TimeSpan(TimeSpan.TicksPerMinute * 5);  // 5 minutes
 
-        /* A synchronous HTTP GET method that returns data received from the sever */
+        /* A synchronous HTTP GET method that returns data received from the sever as a string */
         public string HttpGet(Uri uri)
         {
-            string str = "failed";
             HttpWebRequest request = HttpWebRequest.CreateHttp(uri);
             request.Method = "GET";
-            //RequestState state = new RequestState(request);
-            IAsyncResult asyncResult = request.BeginGetResponse(null, null); // Begin getting response
-            asyncResult.AsyncWaitHandle.WaitOne(BASIC_TIMEOUT); // Wait for 60 seconds for the above action to complete
+            IAsyncResult asyncResult = request.BeginGetResponse(null, null);
+            asyncResult.AsyncWaitHandle.WaitOne(BASIC_TIMEOUT);
             if(asyncResult.IsCompleted)
             {
                 HttpWebResponse response = (HttpWebResponse)request.EndGetResponse(asyncResult); // End getting response
                 Stream stream = response.GetResponseStream();
-                
-                byte[] read = ReadStream(stream);
-                //IAsyncResult asyncResult2 = stream.BeginRead(read, 0,  // Begin reading
-                
-                //stream.Read(byteResponse, 0, byteResponse.Length);
-                UTF8Encoding enc = new UTF8Encoding();
-                str = enc.GetString(read, 0, read.Length);
+                StreamReader streamReader = new StreamReader(stream);
+                string serverResponse = streamReader.ReadToEnd();
+                stream.Dispose();
+                return streamReader.ReadToEnd();
             }
-            return str;
-            //allDone.WaitOne(BASIC_TIMEOUT);
+            else
+            {
+                throw new TimeoutException("The HTTP session has timed out.");
+            }
         }
 
         public void HttpPost(Uri uri)
