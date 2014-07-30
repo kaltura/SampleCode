3bf000aac2e507f023e6268aed55e3f82301fb0c
Evgeny Chistyakov <z_chiz@live.com>
2014-07-29 17:56:12 -0700

    Optional parameters for option constructors

--- cielo24_Ruby/cielo24_gem/lib/cielo24/options.rb
+++ cielo24_Ruby/cielo24_gem/lib/cielo24/options.rb
@@ -49,15 +49,22 @@ module Cielo24
     attr_accessor :replace_slang
     attr_accessor :sound_boundaries
 
-    def initialize
-      @characters_per_caption_line = nil
-      @elementlist_version = nil
-      @emit_speaker_change_token_as = nil
-      @mask_profanity = nil
-      @remove_sounds_list = nil
-      @remove_sound_references = nil
-      @replace_slang = nil
-      @sound_boundaries = nil
+    def initialize(characters_per_caption_line: nil,
+                   elementlist_version: nil,
+                   emit_speaker_change_token_as: nil,
+                   mask_profanity: nil,
+                   remove_sounds_list: nil,
+                   remove_sound_references: nil,
+                   replace_slang: nil,
+                   sound_boundaries: nil)
+      @characters_per_caption_line = characters_per_caption_line
+      @elementlist_version = elementlist_version
+      @emit_speaker_change_token_as = emit_speaker_change_token_as
+      @mask_profanity = mask_profanity
+      @remove_sounds_list = remove_sounds_list
+      @remove_sound_references = remove_sound_references
+      @replace_slang = replace_slang
+      @sound_boundaries = sound_boundaries
     end
   end
 
@@ -71,14 +78,20 @@ module Cielo24
     attr_accessor :time_code_interval
     attr_accessor :timecode_offset
 
-    def initialize
-      @create_paragraphs = nil
-      @newlines_after_paragraph = nil
-      @newlines_after_sentence = nil
-      @timecode_every_paragraph = nil
-      @timecode_format = nil
-      @time_code_interval = nil
-      @timecode_offset = nil
+    def initialize(create_paragraphs: nil,
+                   newlines_after_paragraph: nil,
+                   newlines_after_sentence: nil,
+                   timecode_every_paragraph: nil,
+                   timecode_format: nil,
+                   time_code_interval: nil,
+                   timecode_offset: nil)
+      @create_paragraphs = create_paragraphs
+      @newlines_after_paragraph = newlines_after_paragraph
+      @newlines_after_sentence = newlines_after_sentence
+      @timecode_every_paragraph = timecode_every_paragraph
+      @timecode_format = timecode_format
+      @time_code_interval = time_code_interval
+      @timecode_offset = timecode_offset
     end
   end
 
@@ -116,38 +129,68 @@ module Cielo24
     attr_accessor :strip_square_brackets
     attr_accessor :utf8_mark
 
-    def initialize
-      @build_url = nil
-      @caption_words_min = nil
-      @caption_by_sentence = nil
-      @dfxp_header = nil
-      @disallow_dangling = nil
-      @display_effects_speaker_as = nil
-      @display_speaker_id = nil
-      @force_case = nil
-      @include_dfxp_metadata = nil
-      @layout_target_caption_length_ms = nil
-      @line_break_on_sentence = nil
-      @line_ending_format = nil
-      @lines_per_caption = nil
-      @maximum_caption_duration = nil
-      @merge_gap_interval = nil
-      @minimum_caption_length_ms = nil
-      @minimum_gap_between_captions_ms = nil
-      @minimum_merge_gap_interval = nil
-      @qt_seamless = nil
-      @silence_max_ms = nil
-      @single_speaker_per_caption = nil
-      @sound_threshold = nil
-      @sound_tokens_by_caption = nil
-      @sound_tokens_by_line = nil
-      @sound_tokens_by_caption_list = nil
-      @sound_tokens_by_line_list = nil
-      @speaker_on_new_line = nil
-      @srt_format = nil
-      @srt_print = nil
-      @strip_square_brackets = nil
-      @utf8_mark = nil
+    def initialize(build_url: nil,
+                   caption_words_min: nil,
+                   caption_by_sentence: nil,
+                   dfxp_header: nil,
+                   disallow_dangling: nil,
+                   display_effects_speaker_as: nil,
+                   display_speaker_id: nil,
+                   force_case: nil,
+                   include_dfxp_metadata: nil,
+                   layout_target_caption_length_ms: nil,
+                   line_break_on_sentence: nil,
+                   line_ending_format: nil,
+                   lines_per_caption: nil,
+                   maximum_caption_duration: nil,
+                   merge_gap_interval: nil,
+                   minimum_caption_length_ms: nil,
+                   minimum_gap_between_captions_ms: nil,
+                   minimum_merge_gap_interval: nil,
+                   qt_seamless: nil,
+                   silence_max_ms: nil,
+                   single_speaker_per_caption: nil,
+                   sound_threshold: nil,
+                   sound_tokens_by_caption: nil,
+                   sound_tokens_by_line: nil,
+                   sound_tokens_by_caption_list: nil,
+                   sound_tokens_by_line_list: nil,
+                   speaker_on_new_line: nil,
+                   srt_format: nil,
+                   srt_print: nil,
+                   strip_square_brackets: nil,
+                   utf8_mark: nil)
+      @build_url = build_url
+      @caption_words_min = caption_words_min
+      @caption_by_sentence = caption_by_sentence
+      @dfxp_header = dfxp_header
+      @disallow_dangling = disallow_dangling
+      @display_effects_speaker_as = display_effects_speaker_as
+      @display_speaker_id = display_speaker_id
+      @force_case = force_case
+      @include_dfxp_metadata = include_dfxp_metadata
+      @layout_target_caption_length_ms = layout_target_caption_length_ms
+      @line_break_on_sentence = line_break_on_sentence
+      @line_ending_format = line_ending_format
+      @lines_per_caption = lines_per_caption
+      @maximum_caption_duration = maximum_caption_duration
+      @merge_gap_interval = merge_gap_interval
+      @minimum_caption_length_ms = minimum_caption_length_ms
+      @minimum_gap_between_captions_ms = minimum_gap_between_captions_ms
+      @minimum_merge_gap_interval = minimum_merge_gap_interval
+      @qt_seamless = qt_seamless
+      @silence_max_ms = silence_max_ms
+      @single_speaker_per_caption = single_speaker_per_caption
+      @sound_threshold = sound_threshold
+      @sound_tokens_by_caption = sound_tokens_by_caption
+      @sound_tokens_by_line = sound_tokens_by_line
+      @sound_tokens_by_caption_list = sound_tokens_by_caption_list
+      @sound_tokens_by_line_list = sound_tokens_by_line_list
+      @speaker_on_new_line = speaker_on_new_line
+      @srt_format = srt_format
+      @srt_print = srt_print
+      @strip_square_brackets = strip_square_brackets
+      @utf8_mark = utf8_mark
     end
   end
 
@@ -160,13 +203,18 @@ module Cielo24
     attr_accessor :return_iwp
     attr_accessor :speaker_id
 
-    def initialize
-      @customer_approval_steps = nil
-      @customer_approval_tool = nil
-      @custom_metadata = nil
-      @notes = nil
-      @return_iwp = nil
-      @speaker_id = nil
+    def initialize(customer_approval_steps: nil,
+                   customer_approval_tool: nil,
+                   custom_metadata: nil,
+                   notes: nil,
+                   return_iwp: nil,
+                   speaker_id: nil)
+      @customer_approval_steps = customer_approval_steps
+      @customer_approval_tool = customer_approval_tool
+      @custom_metadata = custom_metadata
+      @notes = notes
+      @return_iwp = return_iwp
+      @speaker_id = speaker_id
     end
   end
 end
