be745b49e9b0658b4eb9b1d351cefe9d3ffcd7fd
Jack <z_chiz@live.com>
2014-12-05 14:40:03 -0800

    EN-2024: started Actions class

---
+++ cielo24_PHP/src/cielo24/Actions.php
@@ -0,0 +1,249 @@
+<?php
+
+class Actions {
+
+    public $BASE_URL = "https://api.cielo24.com";
+    const API_VERSION = 1;
+
+    const LOGIN_PATH = "/api/account/login";
+    const LOGOUT_PATH = "/api/account/logout";
+    const UPDATE_PASSWORD_PATH = "/api/account/update_password";
+    const GENERATE_API_KEY_PATH = "/api/account/generate_api_key";
+    const REMOVE_API_KEY_PATH = "/api/account/remove_api_key";
+    const CREATE_JOB_PATH = "/api/job/new";
+    const AUTHORIZE_JOB_PATH = "/api/job/authorize";
+    const DELETE_JOB_PATH = "/api/job/del";
+    const GET_JOB_INFO_PATH = "/api/job/info";
+    const GET_JOB_LIST_PATH = "/api/job/list";
+    const ADD_MEDIA_TO_JOB_PATH = "/api/job/add_media";
+    const ADD_EMBEDDED_MEDIA_TO_JOB_PATH = "/api/job/add_media_url";
+    const GET_MEDIA_PATH = "/api/job/media";
+    const PERFORM_TRANSCRIPTION = "/api/job/perform_transcription";
+    const GET_TRANSCRIPTION_PATH = "/api/job/get_transcript";
+    const GET_CAPTION_PATH = "/api/job/get_caption";
+    const GET_ELEMENT_LIST_PATH = "/api/job/get_elementlist";
+    const GET_LIST_OF_ELEMENT_LISTS_PATH = "/api/job/list_elementlists";
+
+    public function __construct() { }
+
+    public function __construct1($base_url) {
+        $this->BASE_URL = $base_url;
+    }
+
+    public function __call($name, $arguments) {
+        // TODO
+    }
+
+    /// ACCESS CONTROL ///
+
+    /* Performs a Login action. If useHeaders is true, puts username and password into HTTP headers */
+    public function login($username, $password, $use_headers = false) {
+        $this->assert_argument($username, "Username");
+        $this->assert_argument($password, "Password");
+
+        $query_dict = $this->init_version_dict();
+        $headers = array();
+
+        if (!$use_headers) {
+            $query_dict["username"] = $username;
+            $query_dict["password"] = $password;
+        } else {
+            $headers["x-auth-user"] = $username;
+            $headers["x-auth-key"] = $password;
+        }
+
+        // TODO
+
+        $request_uri = Utils.BuildUri(BASE_URL, LOGIN_PATH, queryDictionary);
+        $server_response = web.HttpRequest(requestUri, HttpMethod.GET, WebUtils.BASIC_TIMEOUT, headers);
+        $response = Utils.Deserialize<Dictionary<string, string>>(serverResponse);
+
+        return $response["ApiToken"];
+    }
+
+    /* Performs a Login action. If useHeaders is true, puts securekey into HTTP headers */
+    public function login1($username, $securekey, $use_headers = false) {
+        $this->assert_argument($username, "Username");
+
+        $query_dict = $this->init_version_dict();
+        $headers = array();
+
+        if (!$use_headers) {
+            $query_dict["username"] = $username;
+            $query_dict["securekey"] = $securekey;
+        } else {
+            $headers["x-auth-user"] = $username;
+            $headers["x-auth-securekey"] = $securekey;
+        }
+
+        // TODO
+        $requestUri = Utils.BuildUri(BASE_URL, LOGIN_PATH, queryDictionary);
+        $serverResponse = web.HttpRequest(requestUri, HttpMethod.GET, WebUtils.BASIC_TIMEOUT, headers);
+        $response = Utils.Deserialize<Dictionary<string, string>>(serverResponse);
+
+        return $response["ApiToken"];
+    }
+
+    /* Performs a Logout action */
+    public function logout($api_token) {
+        $query_dict = $this->init_access_req_dict($api_token);
+
+        //TODO
+        $request_uri = Utils.BuildUri(BASE_URL, LOGOUT_PATH, queryDictionary);
+        web.HttpRequest(requestUri, HttpMethod.GET, WebUtils.BASIC_TIMEOUT); // Nothing returned
+    }
+
+    /* Updates password */
+    public function update_password($api_token, $new_password) {
+        $this->assert_argument($new_password, "New Password");
+
+        $query_dict = $this->init_access_req_dict($api_token);
+        $query_dict["new_password"] = $new_password;
+
+        // TODO
+        $request_uri = Utils.BuildUri(BASE_URL, UPDATE_PASSWORD_PATH, queryDictionary);
+        web.HttpRequest(requestUri, HttpMethod.POST, WebUtils.BASIC_TIMEOUT, Utils.ToQuery(queryDictionary)); // Nothing returned
+    }
+
+    /* Returns a new Secure API key */
+    public function generate_API_key($api_token, $username, $force_new = false) {
+        $this->assert_argument($username, "Username");
+
+        $query_dict = $this->init_access_req_dict($api_token);
+        $query_dict["account_id"] = $username;
+        $query_dict["force_new"] = ($force_new) ? 'true' : 'false';
+
+        // TODO
+        $request_uri = Utils.BuildUri($this->BASE_URL, Actions::GENERATE_API_KEY_PATH, $query_dict);
+        $server_response = web.HttpRequest(requestUri, HttpMethod.GET, WebUtils.BASIC_TIMEOUT);
+        $response = Utils.Deserialize<Dictionary<string, string>>($server_response);
+        return $response["ApiKey"];
+    }
+
+    /* Deactivates the supplied Secure API key */
+    public function remove_API_key($api_token, $api_securekey) {
+        $query_dict = $this->init_access_req_dict($api_token);
+        $query_dict["api_securekey"] = $api_securekey;
+
+        //TODO
+        $request_uri = Utils.BuildUri($this->BASE_URL, Actions::REMOVE_API_KEY_PATH, $query_dict);
+        web.HttpRequest($request_uri, HttpMethod.GET, WebUtils.BASIC_TIMEOUT); // Nothing returned
+    }
+
+    /// JOB CONTROL ///
+
+    /* Creates a new job. Returns an array of Guids where 'JobId' is the 0th element and 'TaskId' is the 1st element */
+    public function create_job($api_token, $job_name = null, $language = "en") {
+
+    }
+
+    /* Authorizes a job with job_id */
+    public function authorize_job($api_token, $job_id) {
+
+    }
+
+    /* Deletes a job with job_id */
+    public function delete_job($api_token, $job_id) {
+
+    }
+
+    /* Gets information about a job with job_id */
+    public function get_job_info($api_token, $job_id) {
+
+    }
+
+    /* Gets a list of jobs */
+    public function get_job_list($api_token) {
+
+    }
+
+    /* Uploads a file from fileStream to job with job_id */
+    public function add_media_to_job($api_token, $job_id, $file_stream) {
+
+    }
+
+    /* Provides job with job_id a url to media */
+    public function add_media_to_job1($api_token, $job_id, $media_url) {
+
+    }
+
+    /* Provides job with job_id a url to media */
+    public function add_embedded_media_to_job($api_token, $job_id, $media_url) {
+
+    }
+
+    /* Helper method for AddMediaToJob and AddEmbeddedMediaToJob methods */
+    private function sendM_media_url($api_token, $job_id, $media_url, $path) {
+
+    }
+
+    /* Returns a Uri to the media from job with job_id */
+    public function get_media($api_token, $job_id) {
+
+    }
+
+    /* Makes a PerformTranscription call */
+    public function perform_transcription($api_token,
+                                         $job_id,
+                                         $fidelity,
+                                         $priority,
+                                         $callback_uri = null,
+                                         $turnaround_hours = null,
+                                         $target_language = null,
+                                         $options = null) {
+
+    }
+
+    /* Returns a transcript from a job with job_id */
+    public function get_transcript($api_token, $job_id, $transcript_options = null) {
+
+    }
+
+    /* Returns a caption from a job with job_id OR if buildUri is true, returns a string representation of the uri */
+    public function get_caption($api_token, $job_id, $caption_format, $caption_options = null) {
+
+    }
+
+    /* Returns an element list */
+    public function get_element_list($api_token, $job_id, $element_list_version = null) {
+
+    }
+
+    /* Returns a list of elements lists */
+    public function get_list_of_element_lists($api_token, $job_id) {
+
+    }
+
+    /// PRIVATE HELPER METHODS ///
+
+    private function get_job_response($api_token, $job_id, $path) {
+
+    }
+
+    /* Returns a dictionary with version, api_token and job_id key-value pairs (parameters used in almost every job-control action). */
+    private function init_job_req_dict($api_token, $job_id) {
+        $this->assert_argument($api_token, "Job ID");
+        $dict = $this->init_access_req_dict($api_token);
+        $dict["job_id"] = $job_id;
+        return $dict;
+    }
+
+    /* Returns a dictionary with version and api_token key-value pairs (parameters used in almost every access-control action). */
+    private function init_access_req_dict($api_token) {
+        $this->assert_argument($api_token, "API Token");
+        $dict = $this->init_version_dict();
+        $dict["api_token"] = $api_token;
+        return $dict;
+    }
+
+    /* Returns a dictionary with version key-value pair (parameter used in every action). */
+    private function init_version_dict() {
+        return array("v" => Actions::API_VERSION);
+    }
+
+    /* If arg is invalid (null or empty), throws an InvalidArgumentException */
+    private function assert_argument($arg, $arg_name) {
+        if ($arg == null) { throw new InvalidArgumentException("Invalid " . $arg_name); }
+        elseif (gettype($arg) == "string" and strlen($arg) == 0) { throw new InvalidArgumentException("Invalid " . $arg_name); }
+    }
+}
