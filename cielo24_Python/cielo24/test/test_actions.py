f61ae0043c5ac4626dd42f393c8b45c583e269a9
Jack <z_chiz@live.com>
2014-12-17 19:37:38 -0800

    EN-2750: python package update (done)

--- cielo24_Python/cielo24/test/test_actions.py
+++ cielo24_Python/cielo24/test/test_actions.py
@@ -1,11 +1,11 @@
 # encoding: utf-8
 from unittest import TestCase
-from traceback import format_exc
 from urlparse import urlparse
 
 from cielo24.actions import Actions
 from cielo24.options import *
 from cielo24.web_utils import *
+from cielo24.enums import *
 
 
 class TestActions(TestCase):
@@ -82,26 +82,26 @@ class TestJob(TestActions):
         if self.job_id is None:
             self.job_id = self.actions.create_job(self.api_token)['JobId']
 
+    # Since all option classes extend BaseOptions class (with all of the functionality) we only need to test one class
     def test_options(self):
-        # TODO: better coverage
-        co = CaptionOptions()
-        co.build_url = True
-        co.dfxp_header = "header"
-        self.assertEqual("build_url=true&dfxp_header=header", co.to_query())
+        options = CaptionOptions()
+        options.build_url = True
+        options.dfxp_header = "header"
+        self.assertEqual("build_url=true&dfxp_header=header", options.to_query())
 
     def test_create_job(self):
-        response = self.actions.create_job(self.api_token, "testname", "en")
+        response = self.actions.create_job(self.api_token, "test_name", Language.English)
         self.assertEqual(32, len(response['JobId']))
         self.assertEqual(32, len(response['TaskId']))
 
     def test_authorize_job(self):
         self.actions.authorize_job(self.api_token, self.job_id)
-        self.job_id = None
+        self.job_id = None  # Create new job in setUp()
 
     def test_delete_job(self):
         self.task_id = self.actions.delete_job(self.api_token, self.job_id)
         self.assertEqual(32, len(self.task_id))
-        self.job_id = None
+        self.job_id = None  # Create new job in setUp()
 
     def test_get_job_info(self):
         response = self.actions.get_job_info(self.api_token, self.job_id)
@@ -120,27 +120,33 @@ class TestJob(TestActions):
         self.assertTrue(isinstance(response, list))
 
     def test_get_media(self):
+        # Add media to job first
+        self.actions.add_media_to_job_url(self.api_token, self.job_id, self.sample_video_url)
+        # Test get media
         media_url = self.actions.get_media(self.api_token, self.job_id)
         parsed_url = urlparse(media_url)
         self.assertIsNot(parsed_url.scheme, '')
         self.assertIsNot(parsed_url.netloc, '')
+        self.job_id = None  # Create new job in setUp()
 
     def test_get_transcript(self):
         self.actions.get_transcript(self.api_token, self.job_id)
 
     def test_get_caption(self):
-        self.actions.get_caption(self.api_token, self.job_id, "SRT")
+        self.actions.get_caption(self.api_token, self.job_id, CaptionFormat.SRT)
 
     def test_get_caption_build_url(self):
-        co = CaptionOptions()
-        co.build_url = True
-        text = self.actions.get_caption(self.api_token, self.job_id, "SRT", co)
-        self.assertTrue(text.__contains__("http"))  # URL must be returned
+        options = CaptionOptions(build_url=True)
+        caption_url = self.actions.get_caption(self.api_token, self.job_id, CaptionFormat.SRT, options)
+        parsed_url = urlparse(caption_url)
+        self.assertIsNot(parsed_url.scheme, '')
+        self.assertIsNot(parsed_url.netloc, '')
+        self.assertTrue(caption_url.__contains__("http"))  # URL must be returned
 
     def test_perform_transcription(self):
         self.task_id = self.actions.add_media_to_job_url(self.api_token, self.job_id, self.sample_video_url)
         self.assertEqual(32, len(self.task_id))
-        self.task_id = self.actions.perform_transcription(self.api_token, self.job_id, "PREMIUM", "STANDARD")
+        self.task_id = self.actions.perform_transcription(self.api_token, self.job_id, Fidelity.PREMIUM, Priority.STANDARD)
         self.assertEqual(32, len(self.task_id))
 
     def test_add_media_to_job_url(self):
